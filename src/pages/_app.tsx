70bb7b92338353fdf318e8660dc0e24cee9d71e1
TATSUMI Soichiro <soichiro.tatsumi@heartbeats.jp>
2023-04-27 16:06:31 +0900

    ライセンス関連のファイルを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,7 @@
+// Copyright 2023 HEARTBEATS Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// Copyright 2023 HEARTBEATS Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -9,5 +13,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
